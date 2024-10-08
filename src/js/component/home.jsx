8d10fa2e95a40edcb075a7bcd3b2c740dcf33943
Diego Barajas <44250354+DiegoCruzzz@users.noreply.github.com>
2024-10-08 01:43:17 +0000

    Semaforo terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,30 @@
-import React from "react";
+import React, { useState } from 'react';
+import '../../styles/index.css'; 
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+const Semaforo = () => {
+  const [luzActiva, setLuzActiva] = useState(null);
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const manejarClick = (luz) => {
+    setLuzActiva(luz);
+  };
+
+  return (
+    <div className="semaforo">
+      <div
+        className={`luz roja ${luzActiva === 'roja' ? 'activa' : ''}`}
+        onClick={() => manejarClick('roja')}
+      />
+      <div
+        className={`luz amarilla ${luzActiva === 'amarilla' ? 'activa' : ''}`}
+        onClick={() => manejarClick('amarilla')}
+      />
+      <div
+        className={`luz verde ${luzActiva === 'verde' ? 'activa' : ''}`}
+        onClick={() => manejarClick('verde')}
+      />
+    </div>
+  );
 };
 
-export default Home;
+export default Semaforo;
+
